be2a10b273ef5f643ab6e597d917239be6281f62
Maedeh Sadat Khorasani <maedeh.skh80@gmail.com>
2025-11-24 14:14:42 +0330

    First update after re-opening project

---
+++ JsProject.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+  let userList = document.querySelectorAll(".name-list");
+  let listInput = document.querySelector(".list-input");
+  let btn = document.querySelector(".addListButtun");
+  btn.addEventListener("click", function () {
+    const newUser = document.createElement("li");
+    const name = document.createTextNode(listInput.value);
+    newUser.appendChild(name);
+    userList.appendChild(newUser);
+    listInput.value = " ";
+  });
+};
+const newname = document.querySelector("list-input");
